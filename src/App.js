d7f730d7809a9a55b17beee000fd622c9eb50e1d
mturcic <mturcicts@gmail.com>
2021-01-10 10:44:45 +0100

    filter-component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,63 @@
 import React, { Component } from 'react';
 import data from './data.json';
 import Products from './components/Products.js';
+import Filter from './components/Filter';
 
 class App extends React.Component {
   constructor() {
     super();
     this.state = {
       products: data.products,
-      size: "",
+      price: "",
       sort: ""
     };
   }
+  //FILTERING PRODUCTS BY PRICE
+  filterProducts = (event) => {
+    console.log(event.target.value);
+    if (event.target.value === ''){
+      this.setState({
+        price: event.target.value, 
+        products: data.products})
+    } else if (event.target.value ==='200'){
+    this.setState({
+      price: event.target.value,
+      products: data.products.filter(
+        (product) => product.price < event.target.value
+      )
+    })
+    } else if (event.target.value ==='500'){
+      this.setState({
+        price: event.target.value,
+        products: data.products.filter(
+          (product) => product.price > 200 && product.price <= event.target.value
+        )
+      })
+    } else {
+      this.setState({
+        price: event.target.value,
+        products: data.products.filter(
+          (product) => product.price >= event.target.value
+        )
+      })
+    }
+  }
+  //SORT PRODUCTS PRICE/DATE ADDED
+  sortProducts = (event) => {
+    const sort = event.target.value;
+    this.setState((state) => ({
+      sort: sort,
+      products: this.state.products
+        .slice()
+        .sort((a, b) =>
+            sort === "LOWEST"
+            ? a.price > b.price ? 1:-1
+            : sort === "HIGHEST"
+            ? a.price < b.price ? 1:-1
+            : a._id > b._id     ? 1:-1
+        ),
+    }));
+  };
   render() {
     return(
     <div className="grid-container">
@@ -20,9 +67,13 @@ class App extends React.Component {
       <main>
         <div className="content">
           <div className="main">
-            <Products products={this.state.products}>
-
-            </Products>
+            <Filter count={this.state.products.length}
+                    price={this.state.price}
+                    sort={this.state.sort}
+                    filterProducts={this.filterProducts}
+                    sortProducts={this.sortProducts}
+            ></Filter>
+            <Products products={this.state.products}></Products>
           </div>
           <div className="sidebar">Cart Items</div>
         </div>
---
+++ src/components/Filter.js
@@ -0,0 +1,26 @@
+import React, { Component } from 'react'
+
+export default class Filter extends Component {
+  render() {
+    return (
+      <div className="filter">
+        <div className="filter-result">{this.props.count+' '}Products</div>
+        <div className="filter-sort">Sort{' '}
+          <select value={this.props.sort} onChange={this.props.sortProducts}>
+            <option>Latest</option>
+            <option value="LOWEST">Price Ascending</option>
+            <option value="HIGHEST">Price Descending</option>
+          </select>
+        </div>
+        <div className="filter-size">Filter{' '}
+          <select value={this.props.price} onChange={this.props.filterProducts}>
+            <option value="">All</option>
+            <option value="200">Under $200</option>
+            <option value="500">$200 to $500</option>
+            <option value="501">Over $500</option>
+          </select>
+        </div>
+      </div>
+    )
+  }
+}
