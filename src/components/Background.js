567e87c9717619d7c4afca794564e429b9f86aca
mturcic <mturcicts@gmail.com>
2021-01-18 16:53:38 +0100

    add-redux-to-cart

---
+++ src/components/Background.js
@@ -0,0 +1,22 @@
+import React, { Component } from "react";
+
+export default class Background extends Component {
+  render() {
+    return (
+      <div>
+        <ul className="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
+      </div>
+    );
+  }
+}
